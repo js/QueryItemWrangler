6c3c4735b28ce16e1729910dfd493be6177fad6c
johsoren <johan.sorensen@finn.no>
2016-06-28 22:09:21 +0200

    Initial Commit

---
+++ QueryItemWrangler/QueryItemWrangler.h
@@ -0,0 +1,19 @@
+//
+//  QueryItemWrangler.h
+//  QueryItemWrangler
+//
+//  Created by Johan Sørensen on 28/06/16.
+//  Copyright © 2016 Johan Sørensen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QueryItemWrangler.
+FOUNDATION_EXPORT double QueryItemWranglerVersionNumber;
+
+//! Project version string for QueryItemWrangler.
+FOUNDATION_EXPORT const unsigned char QueryItemWranglerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QueryItemWrangler/PublicHeader.h>
+
+
