c9be460a581a68c65ca5e9992e83d2e0029dd2f8
johsoren <johan@johansorensen.com>
2016-08-19 10:39:25 +0200

    Move QueryRepresentable into separate file

--- Source/QueryItemWrangler.swift
+++ Source/QueryItemWrangler.swift
@@ -8,49 +8,6 @@
 
 import Foundation
 
-public protocol QueryRepresentable {
-    init?(queryItemValue: String)
-    var queryItemValueRepresentation: String { get }
-}
-
-extension String: QueryRepresentable{
-    public init?(queryItemValue: String) {
-        self.init(queryItemValue)
-    }
-
-    public var queryItemValueRepresentation: String {
-        return self
-    }
-}
-
-extension Int: QueryRepresentable {
-    public init?(queryItemValue: String) {
-        self.init(queryItemValue)
-    }
-
-    public var queryItemValueRepresentation: String {
-        return String(self)
-    }
-}
-
-extension Bool: QueryRepresentable {
-    public init?(queryItemValue: String) {
-        self.init(["1", "true"].contains(queryItemValue))
-    }
-
-    public var queryItemValueRepresentation: String {
-        return self ? "1" : "0"
-    }
-}
-
-public struct QueryItemKey<Key where Key: QueryRepresentable> {
-    internal let key: String
-
-    public init(_ key: String) {
-        self.key = key
-    }
-}
-
 public struct QueryItemWrangler {
     public private(set) var queryItems: [NSURLQueryItem]
 
